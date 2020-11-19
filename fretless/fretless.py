ea3091ee09cc3c6d1b22b1f451736d70fd910982
ErnestoHoro <discusmeniscus@gmail.com>
2020-11-19 17:26:40 +0100

    Alpha version

--- fretless/fretless.py
+++ fretless/fretless.py
@@ -1,45 +1,34 @@
 import copy
+import sys
 from collections.abc import Mapping
 
 import click as click
 import colorama
-import resources_classes
-from resources import TUNINGS, NUMBER_OF_FRETS, NOTE_COLOR_DEFAULT, NOTE_PADDING
+from fretless_printer import FretboardPrinter
+from resources import NUMBER_OF_FRETS, NOTE_COLOR_DEFAULT, NOTE_PADDING
+from resources_classes import Tunings, COctaves
 from termcolor import colored
 from tools import get_frequency_generator, get_notes_generator
 
 
 class Fretless:
+    TUNINGS = Tunings()
+    C_OCTAVES = COctaves()
 
-    def __init__(self, verbose=False, tuning='E-A-D-G-B-E'):
+    def __init__(self, tuning_id='E-A-D-G-B-E', verbose=False):
         colorama.init()
 
-        # assign defaults
-        self.tunings = TUNINGS
         self.number_of_frets = NUMBER_OF_FRETS
         self.note_color_default = NOTE_COLOR_DEFAULT
-        self.note_padding = NOTE_PADDING
 
-        # available text colors: red, green, yellow, blue, magenta, cyan, white
-        self.colored_notes = {
-            'F2': 'green',
-            'C3': 'red',
-            'A#4': 'yellow',
-            'G4': 'cyan',
-        }
+        self.tuning_id = tuning_id
+        self.verbose = verbose
 
-        # (experiment) generic scale (A major)
-        self.colored_notes = {
-            'A': 'red',
-            'B': 'green',
-            'C#': 'green',
-            'D': 'green',
-            'E': 'green',
-            'F#': 'green',
-            'G#': 'green',
-        }
+        self.fretboard_printer = FretboardPrinter()
+        self.fretboard = {}
 
         # (experiment) generic scale (A minor pentatonic)
+        # available text colors: red, green, yellow, blue, magenta, cyan, white
         self.colored_notes = {
             'A': 'red',
             'C': 'green',
@@ -50,18 +39,15 @@ class Fretless:
             'G': 'green',
         }
 
-        self.tuning = tuning
-        self.tuning_notation = self.tunings[tuning]['notation']
-        self.tuning_description = self.tunings[tuning]['description']
-
-        self.fretboard = {}
-
-    def create_fretboard(self, tuning_notation):
-        # Todo: Create them as Dicts rather than Lists.
-        #       Add: :highlight-color, :special-sign, ':special-meaning', etc.
+    def create_fretboard(self):
+        # Todo: Add: :highlight-color, :special-sign, ':special-meaning', etc.
         #       Add: Additional methods to alternate attributes.
         #       Add: ~~Kinda full barre matches or something...
-        #       Add: Map rather real life alternative notes (B♭/b instead of A#). -> look them up
+        tuning_notation = self.TUNINGS.get_notation(self.tuning_id)
+        tuning_description = self.TUNINGS.get_description(self.tuning_id)
+
+        fretboard = {}
+
         f_gen = get_frequency_generator()
         n_gen = get_notes_generator()
 
@@ -75,11 +61,11 @@ class Fretless:
 
             # get note and attributes
             freq_in_hz = next(f_gen)
-            note = next(n_gen)  # Todo: Convert pitch to real-life type (sharp AND flat notation).
+            note = next(n_gen)
             note_long = f'{note}{index_chromatic_scale}'
 
-            # index_note=0 note_long='C0' freq_in_hz='16.35' (1st)
-            # print(f'{index_note=} {note_long=} {freq_in_hz=}')  # Todo: Add logger.debug().
+            if self.verbose:
+                print(f'{index_note=} {note_long=} {freq_in_hz=}')
 
             # update open string indices by tuning
             for i, note_tuning_open in enumerate(tuning_notation):
@@ -88,8 +74,8 @@ class Fretless:
 
             # update fretboard
             for index_string in indices_open_strings:
-                if self.fretboard.get(index_string):
-                    note_id = len(self.fretboard.get(index_string))
+                if fretboard.get(index_string):
+                    note_id = len(fretboard.get(index_string))
                 else:
                     note_id = 0
 
@@ -102,14 +88,16 @@ class Fretless:
                             'note_id': note_id,
                             'freq_hz': freq_in_hz,
                             'color': self._get_color(note_long),
-                            'pitch': self._get_pitch(note_long),
                         }
                     }
                 }
-                self._update_dictionary(self.fretboard, fretboard_update)
+                self._update_dictionary(fretboard, fretboard_update)
 
-        self.fretboard = dict(sorted(self.fretboard.items()))
-        # print(self.fretboard)
+        self.fretboard = dict(sorted(fretboard.items()))
+
+        self.fretboard_printer.fretboard = self.fretboard
+        self.fretboard_printer.tuning_id = self.tuning_id
+        self.fretboard_printer.tuning_description = tuning_description
 
     def _update_dictionary(self, dict_base, dict_update):
         """Update nested dictionary with another dictionary."""
@@ -131,95 +119,21 @@ class Fretless:
         else:
             return self.note_color_default
 
-    @staticmethod
-    def _get_pitch(note_long):
-        if any((char in '#') for char in note_long):
-            return 'sharp'
-        elif any((char in 'b') for char in note_long):
-            return 'flat'
-        else:
-            return ''
-
-    def _print_fretboard_header(self):
-        # title
-        print(colored(f"[ {self.tuning} | {self.tuning_description} ]", attrs=['reverse', 'bold']))
-        print()
-
-        # frets dots
-        for i in range(self.number_of_frets + 1):
-            if i in (3, 5, 7, 9, 15, 17, 19, 21):
-                print('•'.center(self.note_padding) + ' |', end=' ')
-            elif i in (12, 24):
-                print('••'.center(self.note_padding) + ' |', end=' ')
-            else:
-                print(' '.center(self.note_padding) + ' |', end=' ')
-        print()
-
-        # frets indices
-        print(*[str(i).center(self.note_padding) + ' |' for i in range(self.number_of_frets + 1)])
-        print()
-
-    def print_fretboard(self):
-        # Todo: Should optionally also be saved to a file. (generic file_io method)
-        # Todo: Externalize creation of formatted representation to resources_classes.
-
-        # header
-        self._print_fretboard_header()
-
-        # notes
-        for string_id, string_notes in reversed(self.fretboard.items()):
-            for note, attributes in string_notes.items():
-                color = attributes['color']
-                print(colored(note.center(self.note_padding), color) + ' |', end=' ')
-            print()
-
-        print()
-
-        # frequencies
-        for string_id, string_notes in reversed(self.fretboard.items()):
-            for _, attributes in string_notes.items():
-                freq_hz = attributes['freq_hz']
-                color = attributes['color']
-                print(colored(freq_hz.rjust(self.note_padding), color) + ' |', end=' ')
-            print()
-
-        print()
-
-        # pitch
-        for string_id, string_notes in reversed(self.fretboard.items()):
-            for _, attributes in string_notes.items():
-                pitch = attributes['pitch']
-                color = attributes['color']
-                print(colored(pitch.center(self.note_padding), color) + ' |', end=' ')
-            print()
-
-    @classmethod
-    def print_tunings(cls):
-        """Prints tunings available for rendering a fretboard."""
-        print(resources_classes.Tunings().__str__())
-
-    @classmethod
-    def print_c_octaves(cls):
-        """Prints a table showing progression of octaves from C0-C10."""
-        print(resources_classes.COctaves().__str__())
-
     def main(self):
-        self.create_fretboard(self.tuning_notation)
-        self.print_fretboard()
+        self.create_fretboard()
+        self.fretboard_printer.print_fretboard()
 
 
 # -- command line interface & user documentation -----------------------------------------------/100
 
 @click.command(options_metavar='<options>')
-@click.option('--print-tunings', is_flag=True,
-              help='Prints tunings available for rendering a fretboard.')
 @click.option('--print-c-octaves', is_flag=True,
               help='Prints a table showing progression of octaves from C0-C10.')
 @click.option('--verbose', is_flag=True,
               help='Will print verbose/debug messages.')
-# Todo: Add tuning argument (lists should be allowed).
 # Todo: Add export option (--save). Ensure color sequences are removed.
-def cli(print_tunings, print_c_octaves, verbose):
+# Todo: Add signal handler nearby.
+def cli(print_c_octaves, verbose):
     """fretless - A command line music tool
 
     \b
@@ -239,17 +153,22 @@ def cli(print_tunings, print_c_octaves, verbose):
 
     Have a good time and fret less.
     """
-    if verbose:
-        click.echo(f'Passed arguments: {print_tunings=} {print_c_octaves=}')
-
-    if print_tunings:
-        Fretless().print_tunings()
+    fretless = Fretless(verbose=verbose)
 
     if print_c_octaves:
-        Fretless().print_c_octaves()
+        print(fretless.C_OCTAVES)
+        sys.exit()
 
-    if not any((print_tunings, print_c_octaves)):
-        Fretless(verbose).main()
+    while True:
+        print(fretless.TUNINGS)
+
+        choice_ids = click.Choice(fretless.TUNINGS.choice_ids)
+        choice_user = click.prompt('\nPlease select ID', default='0', type=choice_ids)
+
+        fretless.tuning_id = fretless.TUNINGS.get_tuning_id_by_choice_id(choice_id=choice_user)
+        fretless.main()
+
+        input(colored('PRESS KEY TO CONTINUE\n', attrs=['bold']))
 
 
 if __name__ == '__main__':
